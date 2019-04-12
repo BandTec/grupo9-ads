ba2e3fb39116b37d0917656cb4fed0068c6b22d9
Aluno <Aluno@Digital.net>
2019-04-12 15:29:04 -0300

    script novo mysql

---
+++ Banco de Dados/giulia_script.sql
@@ -0,0 +1,71 @@
+create table medicamentos(
+    idMedicamento   int primary key,
+    tipo            varchar(30),
+    tempMax         char(4),    
+    tempMin         char(4),
+    umidMax         char(4),
+    umidMin         char(4)
+);
+
+insert into medicamentos values 
+(1020,'teste',2,2,40,40),
+(1030,'teste',2,2,40,40),
+(1040,'teste',2,2,40,40);
+
+create table usuario(
+    idUser      int primary key,
+    nome        varchar(40),
+    cnpj        char(8),
+    email       varchar(30),
+    senha       varchar(10),
+);
+
+insert into usuario values
+(1,'Dr Fausto',12345678,'fausto@gmail.com','time123'),
+(2,'Dra Mariana',12345678,'mariana@gmail.com','time123'),
+(3,'Dra Vitoria',12345678,'vitoria@gmail.com','time123');
+
+select * from usuario;
+select * from medicamentos;
+
+
+create table geladeira(
+    idGeladeira int primary key,
+    fkUser  int,
+    fkMedicamento   int,
+);
+insert into geladeira values
+(100,1,1040),
+(101,1,1030),
+(102,2,1020),
+(103,3,1020);
+
+select * from geladeira;
+
+alter table geladeira add foreign key (fkUser) references 
+    usuario(idUser);  
+
+alter table geladeira add foreign key (fkMedicamento) references
+    medicamentos(idMedicamento);
+
+create table dados(
+    idTempUmid  int primary key,
+    temp        varchar(4),
+    umid        varchar(4),
+    dat_        date,
+    hora        time,
+    fkGeladeira int
+);
+
+alter table dados add foreign key (fkGeladeira) references
+        geladeira(idGeladeira);
+
+select * from geladeira;
+select* from dados;
+insert into dados values
+(1,1,30,'2014-04-12','16:00',100),
+(2,2,30,'2014-04-12','16:00',101),
+(3,1,30,'2014-04-12','16:00',102);
+
+
+
