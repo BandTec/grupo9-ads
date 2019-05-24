45d66ff350af777b1fc4ef3282e279ddeee08335
giuliamaia2 <giuliamaia2@gmail.com>
2019-05-24 08:16:35 -0300

    tentativa grafico

--- site/public/interno/assets/js/grafico.js
+++ site/public/interno/assets/js/grafico.js
@@ -1,25 +1,116 @@
-let primeiroGrafico = document.getElementById('primeiroGrafico').getContext('2d');                    
-let chart = new Chart(primeiroGrafico, {
-    type: 'line',
-    data: {
-        labels:['19:00', '19:30', '20:00', '20:30', '21:00', '21:30'],
-
-        // eixo y
-        datasets: [
-            {
-                label: 'Temperatura',
-                // eixo x
-                data: [12, 13, 12, 14,13, 11],
-                backgroundColor: "#af2828",
-                borderColor: "#af2828",
-                borderWidth: 3,
-                pointStyle: 'circle',
-                pointRadius: 5,
-                pointHoverRadius: 7,
-                fill: false,
-            }
-        ]
+    // só mexer se quiser alterar o tempo de atualização
+    // ou se souber o que está fazendo!
+    function atualizarGrafico() {
+        obterDadosGrafico();
+        setTimeout(atualizarGrafico, 10000);
     }
 
+    // altere aqui as configurações do gráfico
+    // (tamanhos, cores, textos, etc)
+    function configurarGrafico() {
+        var configuracoes = {
+            responsive: true,
+            animation: exibiu_grafico ? false : {duration: 1500},
+            hoverMode: 'index',
+            stacked: false,
+            title: {
+                display: true,
+                text: 'Histórico recente de temperatura e umidade'
+            },
+            scales: {
+                yAxes: [{
+                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: 'left',
+                    id: 'y-temperatura',
+                }, {
+                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: 'right',
+                    id: 'y-umidade',
 
-});
+                    // grid line settings
+                    gridLines: {
+                        drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                }],
+            }
+        };
+
+        exibiu_grafico = true;
+
+        return configuracoes;
+    }
+
+    // altere aqui como os dados serão exibidos
+    // e como são recuperados do BackEnd
+    function obterDadosGrafico() {
+
+        // neste JSON tem que ser 'labels', 'datasets' etc, 
+        // porque é o padrão do Chart.js
+        var dados = {
+            labels: [],
+            datasets: [
+                {
+                    yAxisID: 'y-temperatura',
+                    label: 'Temperatura',
+                    borderColor: 'red',
+                    backgroundColor: 'red',
+                    fill: false,
+                    data: []
+                },
+                {
+                    yAxisID: 'y-umidade',
+                    label: 'Umidade',
+                    borderColor: 'blue',
+                    backgroundColor: 'blue',
+                    fill: false,
+                    data: []
+                }
+            ]
+        };
+
+        fetch('/leituras/ultimas', { cache: 'no-store' }).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (resposta) {
+
+                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+                    resposta.reverse();
+
+                    for (i = 0; i < resposta.length; i++) {
+                        var registro = resposta[i];
+                    
+                        // aqui, após 'registro.' use os nomes 
+                        // dos atributos que vem no JSON 
+                        // que gerou na consulta ao banco de dados
+
+                        dados.labels.push(registro.hora);
+
+                        dados.datasets[0].data.push(registro.temp);
+                        dados.datasets[1].data.push(registro.umid);
+                    }
+                    console.log(JSON.stringify(dados));
+
+                    plotarGrafico(dados);
+                });
+            } else {
+                console.error('Nenhum dado encontrado ou erro na API');
+            }
+        })
+            .catch(function (error) {
+                console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+            });
+
+    }
+
+    // só altere aqui se souber o que está fazendo!
+    function plotarGrafico(dados) {
+        console.log('iniciando plotagem do gráfico...');
+
+        var ctx = canvas_grafico.getContext('2d');
+        window.canvas_grafico = Chart.Line(ctx, {
+            data: dados,
+            options: configurarGrafico()
+        });
+    }
