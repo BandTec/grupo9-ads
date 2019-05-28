ab3b769f1eefadcfb211a83b4aeda9cab44682ea
giuliamaia2 <giuliamaia2@gmail.com>
2019-05-28 19:45:08 -0300

    inclusao do catch e do timeout

--- site/public/interno/assets/js/dashboards.js
+++ site/public/interno/assets/js/dashboards.js
@@ -1,26 +1,15 @@
+import { SSL_OP_NO_TLSv1_2 } from "constants";
 
 window.onload = function(){
     atualizarGeladeira1();
-    atualizarGeladeira2();
-    atualizarGeladeira3();
 };
-
-function atualizar(){
-    atualizarGeladeira1();
-    atualizarGeladeira2();
-    atualizarGeladeira3();
-}
 
 var t1 = document.getElementById('temp_g1');
 var u1 = document.getElementById('umid_g1');
-var t2 = document.getElementById('temp_g2');
-var u2 = document.getElementById('umid_g2');
-var t3 = document.getElementById('temp_g3');
-var u3 = document.getElementById('umid_g3');
-
-
 var imuno = document.getElementById('div_imuno');
 var s_imuno = document.getElementById('status_imuno');
+
+
 function atualizarGeladeira1(){
     fetch('../leituras/tempo-real1', {cache: 'no-store'}).then(function (response){
         if(response.ok){
@@ -40,42 +29,13 @@ function atualizarGeladeira1(){
         else{
             console.log('Geladeira 1 Não está recebendo dados.');
         }
+    }).catch(function (error){
+        console.error(`O erro é: ${error.message}` );
     });
+ 
+    setTimeout('atualizarGeladeira1()', 8000);
 }
 
 
-function atualizarGeladeira2(){
-    fetch('../leituras/tempo-real2', {cache: 'no-store'}).then(function (response){
-        if(response.ok){
-            console.log('Geladeira 2 Recebendo Dados');
-            response.json().then(function (resposta){
-                console.log(resposta);
-                t2.innerHTML = `${(resposta.temperatura).toFixed(1)}°C`;
-                u2.innerHTML = `${resposta.umidade}%`;
-
-            });
-        }
-        else{
-            console.log('Geladeira 2 Não está recebendo dados.');
-        }
-    });
-}
 
 
-function atualizarGeladeira3(){
-    fetch('../leituras/tempo-real3', {cache: 'no-store'}).then(function (response){
-        if(response.ok){
-            console.log('Geladeira 3 Recebendo Dados');
-            response.json().then(function (resposta){
-                console.log(resposta);
-                t3.innerHTML = `${(resposta.temperatura).toFixed(1)}°C`;
-                u3.innerHTML = `${resposta.umidade}%`;
-
-            });
-        }
-        else{
-            console.log('Geladeira 3 Não está recebendo dados.');
-        }
-    });
-
-}
---
+++ site/public/interno/assets/js/dashboards3.js
@@ -0,0 +1,26 @@
+
+window.onload = function(){
+    atualizarGeladeira3();
+};
+
+
+var t3 = document.getElementById('temp_g3');
+var u3 = document.getElementById('umid_g3');
+
+function atualizarGeladeira3(){
+    fetch('../leituras/tempo-real3', {cache: 'no-store'}).then(function (response){
+        if(response.ok){
+            console.log('Geladeira 3 Recebendo Dados');
+            response.json().then(function (resposta){
+                console.log(resposta);
+                t3.innerHTML = `${(resposta.temperatura).toFixed(1)}°C`;
+                u3.innerHTML = `${resposta.umidade}%`;
+
+            });
+        }
+        else{
+            console.log('Geladeira 3 Não está recebendo dados.');
+        }
+    });
+
+}
