d9775caa704e49928fd46411e430e270499dc5df
unknown <vitoria_cristina_sales@hotmail.com>
2019-06-04 13:44:22 -0300

    Sem o contate-nos no index

--- site/public/js/simulador_financeiro.js
+++ site/public/js/simulador_financeiro.js
@@ -1,7 +1,6 @@
 function simulador(porte, quantidade, vacina){
     var valor_estoque = (Number(quantidade) *  Number(vacina)) * 0.30;
 
-    alert(`Adquirindo o nosso produto, com  R$${porte} de investimento, 
-     é possível diminuir a perca de até R$${(valor_estoque).toFixed(2)} de seu estoque.`);
+    alert(`Adquirindo o nosso produto, com  R$${porte} de investimento, é possível diminuir a perca de até R$${(valor_estoque).toFixed(2)} de seu estoque.`);
 
 }
